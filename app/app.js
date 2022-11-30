4a930f283e419ee8772d34d7b69618d9da0d17ba
Vaman Deshmukh <vamandeshmukh@gmail.com>
2022-11-30 10:42:51 +0530

    c

--- app/app.js
+++ app/app.js
@@ -4,6 +4,25 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
 // Define the `PhoneListController` controller on the `phonecatApp` module
+
+// setTimeout( () => {}, 2000);
+// myApp.controller(arg1, arg2);
+
+let myApp = angular.module('myApp', []);
+
+myApp.controller('MyAppController', function MyAppController($scope) {
+  $scope.apps = [
+    {
+      appName: 'WhatsApp',
+      appVersion: '10.5'
+    },
+    {
+      appName: 'Instagram',
+      appVersion: '20.6'
+    }
+  ];
+});
+
 phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
   $scope.phones = [
     {
